ef61dd382777ae1f3bb48c0b7e36266020559c08
sinmbhel022 <sinmbhel022@student.wethinkcode.co.za>
2023-02-15 15:47:21 +0200

    update to cancel booking functions

--- cancel_booking.py
+++ cancel_booking.py
@@ -18,3 +18,24 @@ def calendar ():
     service = build('calendar', 'v3', credentials=creds)
 
     return service
+
+
+def list_of_vol_slot(email,service):
+    
+    now = datetime.datetime.utcnow().isoformat() + 'Z' 
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            singleEvents=True,
+                                            orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    
+    lis =[]
+    for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            emails = event["attendees"]
+            date_and_time = start.split('T')
+            id = event["id"]
+            title = event['summary']
+            if len(emails) == 2 and emails[1]['email'] == email and title.lower() == "booked":
+                    lis.append((date_and_time[0],date_and_time[1],title,id))
+
+    return lis
